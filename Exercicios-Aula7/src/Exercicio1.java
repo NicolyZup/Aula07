6ddcc6e823221bdb5d9153eeaea33d336ee7eaa6
NicolyZup <nicoly.vitorio@zup.com.br>
2023-05-23 17:03:06 -0300

    Exercicio 1 feito

---
+++ Exercicios-Aula7/src/Exercicio1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe o peso de peixes pescados:");
+        int kilosPeixes = entrada.nextInt();
+
+        if(kilosPeixes>50){
+            int excesso = kilosPeixes - 50;
+            double multa = excesso * 4;
+
+            System.out.printf("O valor da multa pelo excesso de peso é R$%.2f." , multa);
+        } else {
+            System.out.println("Não há multa, peso de peixes dentro do permitido.");
+        }
+    }
+}
